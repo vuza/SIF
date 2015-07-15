5d437658c4d3ba07b16c6c140e0951f08653055c
Matthias Wagner <xon.wagner@yahoo.de>
2015-07-15 15:04:29 +0200

    storyController functions

--- controllers/storyController.js
+++ controllers/storyController.js
@@ -1,5 +1,10 @@
 var storyController = {
-
+    create: function(){},
+    delete: function(){},
+    update: function(){},
+    get: function(){},
+    getMultiple: function(){},
+    rate: function(){}
 };
 
 module.exports = storyController;
