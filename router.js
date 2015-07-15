74ed9c39948aa1d5f22f232895fe3c8102c7e915
Matthias Wagner <xon.wagner@yahoo.de>
2015-07-15 15:01:19 +0200

    routes

--- router.js
+++ router.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var storyController = require('./controllers/storyController');
+var sceneController = require('./controllers/sceneController');
+
 // Routes
-router.get('/test', function(req, res){
-    res.send('Hello World');
-})
+
+// Story
+router.post('/story', storyController.create);
+router.delete('/story', storyController.delete);
+router.put('/story', storyController.update);
+router.get('/story', storyController.get);
+router.get('/stories', storyController.getMultiple);
+router.put('/story/rate', storyController.rate);
+
+//Scenes
+router.post('/scene', sceneController.create);
+router.delete('/scene', sceneController.delete);
+router.put('/scene', sceneController.update);
+router.get('/scene', sceneController.get);
+router.get('/scenes', sceneController.getMultiple);
 
 module.exports = router;
